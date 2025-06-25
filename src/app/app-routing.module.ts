e3bffc436449319d0ce8745f20cbdacc331e13cc
nghilinh2802 <linhldn22411c@st.uel.edu.vn>
2025-06-26 01:39:07 +0700

    đồng bộ giao diện

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,6 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { RoleManagementComponent } from './components/role-management/role-management.component';
 import { LoginComponent } from './components/login/login.component';
-import { DataComponent } from './components/data/data.component';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
 import { OrderComponent } from './components/order/order.component';
 import { OrderViewComponent } from './components/order-view/order-view.component';
@@ -27,22 +26,22 @@ import { FlashsaleManagementComponent } from './components/flashsale/flashsale.c
 import { ChatComponent } from './components/chat/chat.component';
 
 const routes: Routes = [
-  { path: 'data', component: DataComponent },
+
   { path: 'login', component: LoginComponent },
   { path: 'role-management', component: RoleManagementComponent, canActivate: [AdminGuard] },
   { path: 'sidebar', component: SidebarComponent },
-  { path: 'order', component: OrderComponent },
+  { path: 'order', component: OrderComponent, canActivate: [AdminGuard] },
   { path: 'order/:id', component: OrderViewComponent },
   { path: 'order-update/:id', component: OrderUpdateComponent },
-  { path: 'blogs', component: BlogsComponent },
-  { path: 'voucher-management', component: VoucherComponent },
-  { path: 'promotion-management', component: PromotionsComponent },
+  { path: 'blogs', component: BlogsComponent, canActivate: [AdminGuard] },
+  { path: 'voucher-management', component: VoucherComponent, canActivate: [AdminGuard] },
+  { path: 'promotion-management', component: PromotionsComponent, canActivate: [AdminGuard] },
   { path: 'product-management', component: ProductManagementComponent, canActivate: [AdminGuard] },
   { path: 'add-product', component: AddProductModalComponent },
   { path: 'edit-product/:id', component: EditProductModalComponent },
   { path: 'product-details/:id', component: ProductDetailsModalComponent },
-  { path: 'flashsale', component: FlashsaleManagementComponent},
-  { path: 'chat', component: ChatComponent},
+  { path: 'flashsale', component: FlashsaleManagementComponent, canActivate: [AdminGuard]},
+  { path: 'chat', component: ChatComponent, canActivate: [AdminGuard]},
   { path: 'navbar', component: NavbarComponent },
   { path: 'dashboard', component: DashboardComponent },
   { path: 'report', component: ReportComponent, canActivate: [AdminGuard] },
